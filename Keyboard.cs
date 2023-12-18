ae03ac1f0a09e6214b760457f4afbe1de5e13d37
carlxw <87335263+carlxw@users.noreply.github.com>
2023-12-18 13:42:39 -0500

    Remove redundant function to send a literal string as keyboard input

--- Keyboard.cs
+++ Keyboard.cs
@@ -11,10 +11,4 @@ class Keyboard
         if (repeat == 0) SendKeys.SendWait(str);
         else for (uint i = 0; i < repeat; i++) SendKeys.SendWait(str);
     }
-
-    /// <summary>
-    /// Send input string to system
-    /// </summary>
-    /// <param name="str">Character or string of characters to send</param>
-    public static void sendStr(string str) { for (int i = 0; i < str.Length; i++) SendKeys.SendWait(str[i].ToString()); }
 }
